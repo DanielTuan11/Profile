6c81a05d19b292dd0ae468d83ec51c8de07ef0c5
Tuan, Daniel (Dang Quoc Tuan Data and Digital) <Daniel.Tuan@deheus.com>
2025-04-04 14:30:20 +0700

    fix loi

--- build/precache-manifest.5fa22d3a9752ae21dacfb38abeae87a4.js
+++ build/precache-manifest.5fa22d3a9752ae21dacfb38abeae87a4.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb31f96106d339b33d1eb8b5e7515fe0",
+    "revision": "17456d00227948341774a84da45e6ab9",
     "url": "/Profile/index.html"
   },
   {
-    "revision": "1be6ff8919fa899065d5",
+    "revision": "2c082ae116b119687f92",
     "url": "/Profile/static/css/2.b71989ea.chunk.css"
   },
   {
-    "revision": "43f4e836c032447e7dfb",
+    "revision": "2b2cb3b382917c52bf0a",
     "url": "/Profile/static/css/main.c618effe.chunk.css"
   },
   {
-    "revision": "1be6ff8919fa899065d5",
-    "url": "/Profile/static/js/2.eb26b729.chunk.js"
+    "revision": "2c082ae116b119687f92",
+    "url": "/Profile/static/js/2.676297fb.chunk.js"
   },
   {
     "revision": "28900bc64ebee1095657935b0a3ae3da",
-    "url": "/Profile/static/js/2.eb26b729.chunk.js.LICENSE.txt"
+    "url": "/Profile/static/js/2.676297fb.chunk.js.LICENSE.txt"
   },
   {
     "revision": "87e84beceadd106596cf",
     "url": "/Profile/static/js/3.f725a25d.chunk.js"
   },
   {
-    "revision": "43f4e836c032447e7dfb",
-    "url": "/Profile/static/js/main.b1ed86bf.chunk.js"
+    "revision": "2b2cb3b382917c52bf0a",
+    "url": "/Profile/static/js/main.a5c0c8a9.chunk.js"
   },
   {
     "revision": "8f215142d26ae11788c5",
